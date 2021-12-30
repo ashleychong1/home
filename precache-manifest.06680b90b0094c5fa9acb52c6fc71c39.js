bfe81edf7225a3a69c0f2a5c58514bc72618c79f
Ashley Chong <akchong@bu.edu>
2021-12-30 18:24:58 -0500

    Updates

---
+++ precache-manifest.06680b90b0094c5fa9acb52c6fc71c39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0a17fe122826101a41309ca25818e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3e1f89559f64a3571af",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e1f89559f64a3571af",
+    "url": "/static/js/main.8c8e271a.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "cb35e5f6ef0db6e341a01b5f365919a7",
+    "url": "/static/media/face.cb35e5f6.png"
+  },
+  {
+    "revision": "272bfe982d0692a33496ff2895083452",
+    "url": "/static/media/resume.272bfe98.pdf"
+  }
+]);
